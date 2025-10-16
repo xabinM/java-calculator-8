4bad9f7f2e6c1d7a2b88765600688c68284c722b
xabinM <binine2@naver.com>
2025-10-16 14:19:38 +0900

    feat: 기본 구분자로 split 후 더하기 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,31 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+
+        String input = Console.readLine();
+
+        System.out.println(calculator(input));
+    }
+
+    private static int calculator(String input) {
+
+        int[] splitInput = Arrays.stream(input.split("[,:]"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int result = 0;
+        for (int v : splitInput) {
+
+            result += v;
+        }
+
+        return result;
     }
 }
